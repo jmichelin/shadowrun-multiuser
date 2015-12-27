b7d112707b98b8ed931c16be6a11538b3facfc9f
jmichelin <jmichelinphone@gmail.com>
2015-12-27 14:51:06 -0800

    gets modal edit working on individual chars

--- app/welcome/welcome.js
+++ app/welcome/welcome.js
@@ -15,24 +15,58 @@ angular.module('myApp.welcome', ['ngRoute'])
   var sync = $firebase(firebaseObj);
   $scope.characters = sync.$asArray();
 
+  $scope.editCharacter = function(id) {
+    var firebaseObj = new Firebase("https://shadowrun-multiuser.firebaseio.com/characters/" + id);
+    var sync = $firebase(firebaseObj);
+    $scope.charToUpdate = sync.$asObject();
+    console.log("+++ welcome.js line 22 $scope.charToUpdate => ", $scope.charToUpdate);
+    $('#editChar').modal(); //trigger modal popup
+  }
+
+  $scope.update = function () {
+    console.log($scope.charToUpdate.$id);
+    var firebaseObj = new Firebase("https://shadowrun-multiuser.firebaseio.com/characters/" + $scope.charToUpdate.$id);
+    var character = $firebase(firebaseObj);
+    character.$update({
+      //massive update object
+      //userID: playerID,
+      Name: $scope.charToUpdate.Name,
+      Body: $scope.charToUpdate.Body,
+      Agility: $scope.charToUpdate.Agility,
+      Reaction: $scope.charToUpdate.Reaction,
+      Strength: $scope.charToUpdate.Strength,
+      Charisma: $scope.charToUpdate.Charisma,
+      Intuition: $scope.charToUpdate.Intuition,
+      Logic: $scope.charToUpdate.Logic,
+      Willpower: $scope.charToUpdate.Willpower,
+      Edge: $scope.charToUpdate.Edge,
+      Essence: $scope.charToUpdate.Essence,
+      Initiative: $scope.charToUpdate.Initiative,
+      InitiativePasses: $scope.charToUpdate.InitiativePasses,
+      Blades: $scope.charToUpdate.Blades,
+      Clubs: $scope.charToUpdate.Clubs,
+      ExoticMelee: $scope.charToUpdate.ExoticMelee,
+      Unarmed: $scope.charToUpdate.Unarmed,
+      Archery: $scope.charToUpdate.Archery,
+      Automatics: $scope.charToUpdate.Automatics,
+      ExoticRanged: $scope.charToUpdate.ExoticRanged,
+      Heavy: $scope.charToUpdate.Heavy,
+      Longarms: $scope.charToUpdate.Longarms,
+      Pistols: $scope.charToUpdate.Pistols,
+      Throwing: $scope.charToUpdate.Throwing,
+      ArmorRating: $scope.charToUpdate.ArmorRating,
+      Dodge: $scope.charToUpdate.Dodge,
+      Gymnastics: $scope.charToUpdate.Gymnastics,
+      Physical: $scope.charToUpdate.Physical
+
+      // end massive update object
+    }).then(function(ref){
+      console.log(ref.key()); //bar?
+      $('#editChar').modal('hide');
+    }, function(error){
+      console.log("Error: ", error);
+    });
+  }
+
 
 }]);
-
-
-
-
-/* original way
-var ref = new Firebase('https://shadowrun-multiuser.firebaseio.com/characters');
-var sync = $firebase(firebaseObj);
-$scope.articles = sync.$asArray();
-
-
-
-
-.controller('WelcomeCtrl', ['$scope', '$firebaseArray', 'CommonProp' function($scope,$firebaseArray,CommonProp) {
-  $scope.username = CommonProp.getUser();
-  var ref = new Firebase('https://shadowrun-multiuser.firebaseio.com/characters');
-  $scope.characters = $firebaseArray(ref);
-}]);
-
-*/
